82af4941cbbd517d7cd7f7b8c1593c78510c58a8
nimcoam <120413753+nimcoam@users.noreply.github.com>
2022-12-13 23:58:13 +0300

    Create nimco.py
    Oh, this is one of my projects and I hope to do better. I would like to hear your feedback and learn more.
    Thank you!

---
+++ nimco.py
@@ -0,0 +1,21 @@
+print("Hello, World!")
+name = input('Please enter your name: ')
+def multiNumbers(a,b,c,d):
+     m = a*b+c-d
+     return m
+march=multiNumbers(1,9,9,7)
+print(march)
+a=25
+while a>=16:
+    print(a)
+    a=a-9
+    print("hey, you can work!")
+    print(a)
+else:
+    print("oh! i'm sorry, let' play rock, paper, scissors")
+    
+import random
+
+mylist = ["rock", "paper", "scissors"]
+
+print(random.choice(mylist))
